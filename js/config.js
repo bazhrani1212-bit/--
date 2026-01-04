19506c3a7ce38af8fb820f2cabffdebed2a37861
bazhrani1212-bit <bazhrani1212@gmail.com>
2026-01-05 00:06:25 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxMZOvfhuzGY3Unz2jqyFVfSLkvN5xQKxuXyOqpUV9hkdcokD4vxjjDxjZtqkoWzvir/exec";
+export const APP_TOKEN = ""; // اختياري
+export const LEVEL_WORDS = ["متفوق","جيد","يحتاج متابعة","تحسن بتفوق","ضعيف جدا","ضعيف","جيد وبحتاج متابعة"];
+export const ACTION_TYPES = ["جلسة علاجية فردية","جلسة علاجية جماعية","واجب علاجي","خطة إثرائية","تعلّم تعاوني","تعليم مقلوب","أنشطة تطبيقية","تدريب إضافي","غير ذلك"];
+export const MONTHS_AR = ["يناير","فبراير","مارس","أبريل","مايو","يونيو","يوليو","أغسطس","سبتمبر","أكتوبر","نوفمبر","ديسمبر"];
+export const YEARWORK_SCHEMA = { p1A: 40, p1B: 20, p2A: 40, p2B: 20, final: 40 };
